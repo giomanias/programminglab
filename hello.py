58394be216daa2892b5895f7cb9f71ba95725ab7
GioManias <74066165+giomanias@users.noreply.github.com>
2021-11-16 16:57:48 +0000

    esercizio lezione 2

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello world")
+#srivi una funzione che torni la somma degli elementi di una lista
+
+def somma_lista(lista):
+  return sum(my_list)
+
+my_list=[1,2,3,4,5]
+risultato=somma_lista(my_list)
+print(risultato)
